e1c473cc833cf9217068e51a78cd4b3e8e39c270
arham choudhary <arham@localhost.localdomain>
2021-08-05 18:36:11 +0530

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const expressip = require('express-ip');
 const PORT = process.env.PORT || 7000;
-const path = require('path');
 
 app.use(expressip().getIpInfoMiddleware);
 
@@ -14,6 +13,6 @@ app.get('/', function (req, res) {
 });
 
 app.listen(app.get('PORT'), function () {
-    console.log('Express started on http://localhost:' +
+    console.log('started on http://localhost:' +
         app.get('PORT') + '; press Ctrl-C to terminate.');
 });
